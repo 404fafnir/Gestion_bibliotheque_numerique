57191d9f7e3e6f2651dbf843407d3a884286af50
Fafnir <26090074+404fafnir@users.noreply.github.com>
2025-10-21 16:43:58 +0200

    Update Gestion_bibliotheque_numerique.py

--- Gestion_bibliotheque_numerique.py
+++ Gestion_bibliotheque_numerique.py
@@ -1,1 +1,25 @@
 # Système de Gestion Bibliothèque Numérique
+
+class Livre:
+    def __init__(self, identifiant, titre, auteur, categorie, nbexemplaires, status):
+        self.identifiant = identifiant
+        self.titre = titre
+        self.auteur = auteur
+        self.categorie = categorie
+        self.nbexemplaires = nbexemplaires
+        self.status = status
+    pass
+
+class Utilisateur:
+    pass
+
+class Lecteur(Utilisateur):
+    pass
+
+class Bibliothecaire(Utilisateur):
+    pass
+
+class Bibliotheque:
+    pass
+
+# Système de Gestion Bibliothèque Numérique
