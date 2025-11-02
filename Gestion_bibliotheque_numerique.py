added69f4f8c8c58e0331d7c6b25cd13410fad17
Hugo Hernandez <hugo.ac.hernandez@gmail.com>
2025-11-02 02:37:22 +0100

    Ajout de l'Interface graphique

--- Gestion_bibliotheque_numerique.py
+++ Gestion_bibliotheque_numerique.py
@@ -51,7 +51,7 @@ class Bibliotheque:
         else:
             return False
 
-    def ajouter_livres(self,livre, utilisateur):
+    def ajouter_livres(self, livre, utilisateur):
         if not self.autorisation(utilisateur):
             print('/!\ Accès refusé.')
             return
@@ -107,17 +107,17 @@ class Bibliotheque:
 
     def ajouter_utilisateur(self, utilisateur):
         if utilisateur.identifiant in self.utilisateurs:
-            print(f"/!\\ Un utilisateur avec l'ID {utilisateur.identifiant} existe déjà.")
+            print(f"/!\ Un utilisateur avec l'ID {utilisateur.identifiant} existe déjà.")
             return
         self.utilisateurs[utilisateur.identifiant] = utilisateur
         print(f"Utilisateur ajouté : {utilisateur.nom} ({utilisateur.usertype})")
 
     def supprimer_utilisateur(self, utilisateur):
         if utilisateur.identifiant not in self.utilisateurs:
-            print("/!\\ Utilisateur introuvable.")
+            print("/!\ Utilisateur introuvable.")
             return
         del self.utilisateurs[utilisateur.identifiant]
-        print(f"Utilisateur supprimé : {utilisateur.nom}")
+        print("Utilisateur supprimé !")
 
     def emprunter_livre(self, livre, utilisateur):
         if not isinstance(utilisateur, Lecteur):
@@ -178,3 +178,132 @@ class Bibliotheque:
         print(f"Nombre total de livres : {total_livres}")
         print(f"Nombre total de lecteurs : {total_lecteurs}")
         print(f"Nombre total de livres empruntés : {total_emprunts}")
+
+biblio = Bibliotheque()
+admin = Bibliothecaire(1, "admin", "admin@test.com")
+exempleLecteur = Lecteur(2, "Jean Dupont", "jean.dupont@test.com")
+exempleLivre = Livre("Les Contemplations", "Victor Hugo", "Poèmes", 10)
+
+
+while True:
+    print("\n--- Menu Principal ---")
+    print("1. Gérer les livres")
+    print("2. Gérer les utilisateurs")
+    print("3. Gérer les emprunts")
+    print("4. Quitter")
+    choix = int(input(""))
+    match choix:
+        case 1:
+            print("\n--- Gérer les livres ---")
+            print("1. Ajouter livre")
+            print("2. Modifier livre")
+            print("3. Supprimer livre")
+            print("4. Afficher liste complète des livres")
+            print("5. Rechercher un livre")
+            print("6. Revenir au menu principal")
+            sousChoix = int(input(""))
+            match sousChoix:
+                case 1:
+                    titre = input("Entrer le titre du livre :")
+                    auteur = input("Entrer l'auteur du livre :")
+                    categorie = input("Entrer la catégorie principale du livre :")
+                    nombre = int(input("Entrer le nombre d'exemplaire du livre :"))
+                    nouveauLivre = Livre(titre, auteur, categorie, nombre)
+                    biblio.ajouter_livres(nouveauLivre, admin)
+                    pass
+                case 2:
+                    attribut = input("Que voulez vous modifiez :")
+                    valeur = input("Entrer la modification :")
+                    biblio.modifier_livres(exempleLivre, (attribut, valeur), admin)
+                    pass
+                case 3:
+                    biblio.supprimer_livres(exempleLivre, exempleLecteur)
+                    pass
+                case 4:
+                    print("\nVoici la liste complète des livres :")
+                    biblio.afficher_livres()
+                    pass
+                case 5:
+                    attribut = input("Entrer selon quel critère vous voulez chercher le livre :")
+                    valeur = input(f"Entrez {attribut} du livre que vous chercher :")
+                    biblio.rechercher_livres((attribut, valeur))
+                    pass
+                case 6:
+                    pass
+                case _:
+                    print("Choix invalide !")
+                    pass
+        case 2:
+            print("\n--- Gérer les utilisateurs ---")
+            print("1. Ajouter un utilisateur")
+            print("2. Supprimer un utilisateur")
+            print("3. Revenir au menu principal")
+            sousChoix = int(input(""))
+            match sousChoix:
+                case 1:
+                    id = int(input("Entrer l'id de l'utilisateur :"))
+                    nom = input("Entrer le nom de l'utilisateur :")
+                    mail = input("Entrer le mail de l'utilisateur :")
+                    typeUtilisateur = int(input("L'utilisateur est un lecteur (si oui 1) :"))
+                    if typeUtilisateur == 1:
+                        nouvelUtilisateur = Lecteur(id, nom, mail)
+                    else:
+                        nouvelUtilisateur = Bibliothecaire(id, nom, mail)
+                    biblio.ajouter_utilisateur(nouvelUtilisateur)
+                    pass
+                case 2:
+                    mail = input("Mail de l'utilisateur à supprimer :")
+                    for u in biblio.utilisateurs:
+                        if u.email == mail:
+                            biblio.supprimer_utilisateur(u)
+                            pass
+                    print("/!\ Utilisateur introuvable !")
+                    pass
+                case 3:
+                    pass
+                case _:
+                    print("Choix invalide !")
+                    pass
+        case 3:
+            print("\n--- Gérer les emprunts ---")
+            print("1. Emprunt")
+            print("2. Retour")
+            print("3. Revenir au menu principal")
+            sousChoix = int(input(""))
+            match sousChoix:
+                case 1:
+                    mail = input("Entrer le mail du lecteur qui veut emprunter :")
+                    utilisateur = None
+                    livre = None
+                    for u in biblio.utilisateurs:
+                        if u.email == mail:
+                            utilisateur = u
+                    titre = input("Entrer le titre du livre à emprunter :")
+                    for l in biblio.livres:
+                        if l.titre == titre:
+                            livre = l
+                    biblio.emprunter_livre(livre, utilisateur)
+                    pass
+                case 2:
+                    mail = input("Entrer le mail du lecteur qui rend le livre :")
+                    utilisateur = None
+                    livre = None
+                    for u in biblio.utilisateurs:
+                        if u.email == mail:
+                            utilisateur = u
+                    titre = input("Entrer le titre du livre rendu :")
+                    for l in biblio.livres:
+                        if l.titre == titre:
+                            livre = l
+                    biblio.rendre_livres(livre, utilisateur)
+                    pass
+                case 3:
+                    pass
+                case _:
+                    print("Choix invalide !")
+                    pass
+        case 4:
+            break
+        case _:
+            print("Choix invalide !")
+            pass
